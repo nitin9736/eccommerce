f4d70346350d022eb2b410f9a9eaa87be7759e9e
Nitin Nimbalkar <nitinnimbalkar536@gmail.com>
2019-08-17 14:50:15 +0530

    added components and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Directory from '../../components/directory/directory.component'
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <Directory />
+        </div>
+    </div>
+);
+
+export default HomePage;
