39a66201d34f2b999e1800b130ba3b301ccb6eda
Nitin Nimbalkar <nitinnimbalkar536@gmail.com>
2019-08-18 00:42:53 +0530

    Added header component and shop page added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,13 @@
+import React from 'react';
 
-import React from "react";
-import Directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
 const HomePage = () => (
-    <div className="homepage">
-        <div className="directory-menu">
-            <Directory />
-        </div>
-    </div>
+  <div className='homepage'>
+    <Directory />
+  </div>
 );
 
 export default HomePage;
